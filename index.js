bfe18783358fdbabc43633cef05573903e3f504c
Katie Blizzard <kblizzard11@gmail.com>
2019-01-18 02:21:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,8 +40,40 @@ document.addEventListener("DOMContentLoaded", (e) => {
  
  const titanic = document.getElementById("Titanic");
  titanic.addEventListener("click", (e) => {
-    alert("Titanic!");
+    
+   
+   document.getElementById("title").innerHTML = movies.Titanic.title;
+ document.getElementById("director").innerHTML = movies.Titanic.director;
+ document.getElementById("genre").innerHTML = movies.Titanic.genre;
+ document.getElementById("filmRating").innerHTML = movies.Titanic.filmRating;
+ document.getElementById("poster").setAttribute("src", movies.Titanic.poster);
+ document.getElementById("audienceScore").innerHTML = movies.Titanic.audienceScore;
+ document.getElementById("description").innerHTML = movies.Titanic.description;
  
+
+ 
+ 
+ for (let i = 0; i < movies.Titanic.cast.length; i++) {
+   for (key in movies.Titanic.cast[i]) {
+     console.log(movies.Titanic.cast[i][key])
+   }} 
+
+
+ for (let i = 0; i < movies.Titanic.cast.length; i++) {
+     let JSONrole = movies.Titanic.cast[i]["role"];
+     let JSONactor = movies.Titanic.cast[i]["actor"];
+     let output = JSONrole + ": " + JSONactor + "<br><br>";
+     document.getElementById("cast").innerHTML += output;
+   } 
+
+
+ for (let i = 0; i < movies.Titanic.reviews.length; i++) {
+     let JSONusername = movies.Titanic.reviews[i]["username"];
+     let JSONcontent = movies.Titanic.reviews[i]["content"];
+     let output = JSONusername + ": " + JSONcontent + "<br><br>";
+     document.getElementById("reviews").innerHTML += output;
+   }
+   
  })
  
  const terminator = document.getElementById("Terminator 2");
