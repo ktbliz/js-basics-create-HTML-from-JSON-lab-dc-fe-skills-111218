dc85d46ab3b28d69d38247677879a5e5cfa0a406
Katie Blizzard <kblizzard11@gmail.com>
2019-01-18 01:53:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,69 @@
-document.addEventListener("DOMContentloaded", (e) => {
-  // invoking a function here will make sure it happens on page load
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", (e) => {
+  
+ document.getElementById("title").innerHTML = movies.Titanic.title;
+ document.getElementById("director").innerHTML = movies.Titanic.director;
+ document.getElementById("genre").innerHTML = movies.Titanic.genre;
+ document.getElementById("filmRating").innerHTML = movies.Titanic.filmRating;
+ document.getElementById("poster").setAttribute("src", movies.Titanic.poster);
+ document.getElementById("audienceScore").innerHTML = movies.Titanic.audienceScore;
+ document.getElementById("description").innerHTML = movies.Titanic.description;
+ 
+
+ 
+ 
+ for (let i = 0; i < movies.Titanic.cast.length; i++) {
+   for (key in movies.Titanic.cast[i]) {
+     console.log(movies.Titanic.cast[i][key])
+   }} 
+
+/*
+ for (let i = 0; i < movies.Titanic.cast.length; i++) {
+   for (key in movies.Titanic.cast[i]) {
+     let JSONrole = movies.Titanic.cast[i]["role"]; 
+     let JSONactor = movies.Titanic.cast[i]["actor"];
+     let output = JSONrole + ": " + JSONactor + "<br>";
+     document.getElementById("cast").innerHTML += output;
+   }} 
+*/
+
+for (let i = 0; i < movies.Titanic.cast.length; i++) {
+   for (key in movies.Titanic.cast[i]) {
+     if (key = "role") {
+      let JSONrole = movies.Titanic.cast[i]["role"];}
+    else if (key = "actor") {
+      let JSONactor = movies.Titanic.cast[i]["actor"];}
+    let output = JSONrole + ": " + JSONactor + "<br>";
+     document.getElementById("cast").innerHTML += output;
+   }} 
+
+/*
+     if (movies.Titanic.cast[i].key = "role") {
+       role = movies.Titanic.cast[i][key];
+     } else if (movies.Titanic.cast[i].key = "actor") {
+       actor = movies.Titanic.cast[i][key];
+   } output += role + actor; 
+ }
+ 
+*/
+ 
+document.getElementById("cast").appendChild
+
+ document.getElementById("reviews").innerHTML = "reviews"
+ 
+ })
+ 
+ 
+ const titanic = document.getElementById("Titanic");
+ titanic.addEventListener("click", (e) => {
+    alert("Titanic!");
+ 
+ })
+ 
+ const terminator = document.getElementById("Terminator 2");
+ terminator.addEventListener("click", (e) => {
+    alert("Terminator!")
+ 
+ })
+
+
+
